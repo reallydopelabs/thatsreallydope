07bef5da04dc2352b66978d05fb083c09552266a
Sam Rapaport <me@samrapaport.com>
2024-02-02 13:10:19 -0800

    Remove console log.

--- src/lib/strapi.js
+++ src/lib/strapi.js
@@ -1,7 +1,6 @@
 export const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL
 
 export function getUploadUrlFromStrapiData(data, format) {
-  console.log(data, format)
   return format
     ? `${data.attributes.formats[format].url}`
     : `${data.attributes.url}`
